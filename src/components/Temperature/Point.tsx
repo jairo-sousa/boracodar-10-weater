b62b130ce861b427f259ac7b02ae5a65df017347
jairo sousa <jairo.sousa.rufino@gmail.com>
2023-03-10 13:40:13 -0300

    style(Point): fix margin rigth

--- src/components/Temperature/Point.tsx
+++ src/components/Temperature/Point.tsx
@@ -2,7 +2,7 @@ import { Flex, Img, Text } from "@chakra-ui/react";
 
 export function Point() {
 	return (
-		<Flex w="100%" mt="2.3rem" gap="0.833rem" justify="end">
+		<Flex w="100%" mt="2.3rem" mr="2rem" gap="0.833rem" justify="end">
 			<Img src="/point.svg" />
 			<Text color="#C2BFF4" fontSize="1.4rem">
 				Rio do Sul, SC
